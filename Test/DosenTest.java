35d8ad2d27bb789a753f79620ac3e154d2c477dd
rmdn221299 <ramm.dani221299@yahoo.com>
2021-03-12 00:49:42 +0700

    krs polneloh

--- Test/DosenTest.java
+++ Test/DosenTest.java
@@ -5,8 +5,8 @@ public class DosenTest {
     public static void main(String [] args){
         ModelDosen dsn = new ModelDosen();
         
-        //Test 1 - Uji panjang NIDN != 7
-        String hasilEkspestasi = "Panjang NIDN harus 7!";
+        //Test 1 - Menguji panjang karakter 
+        String hasilEkspestasi = "Panjang NIDN harus 7 karakter !";
         if(dsn.setNidn("12345678") == hasilEkspestasi){
             System.out.println("Test 1 - Berhasil");
         }else{
@@ -22,7 +22,7 @@ public class DosenTest {
         }
         
         //Test 3 - NIDN harus angka
-        String hasilEkspestasi3 = "NIDN harus berisi angka!";
+        String hasilEkspestasi3 = "NIDN harus angka!";
         if(dsn.setNidn("i2e4567") == hasilEkspestasi3){
             System.out.println("Test 1 - Berhasil");
         }else{
